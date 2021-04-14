1d2bea763af9b83878779d708c956f19373faa7f
shubham-dreamer <gns7161356@gmail.com>
2021-04-14 23:42:42 +0530

    retrieve data, fetch post, stylish profile,delete and edit functionality done

--- home.php
+++ home.php
@@ -36,7 +36,7 @@ if(!isset($_SESSION['user_email'])){
 <div class="row">
 	<div class="col-sm-12">
 		<center><h2><strong>News Feed</strong></h2><br></center>
-		<!-- <?php //echo get_posts(); ?> -->
+		<?php echo get_posts(); ?> 
 	</div>
 </div>
 
