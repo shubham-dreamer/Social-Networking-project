d9ff48a529ddce3c8c64ec3d8f68b174d30699d0
shubham-dreamer <gns7161356@gmail.com>
2021-04-15 23:35:01 +0530

    comment,viewpost,search,display,userprofile functinality completed

--- single.php
+++ single.php
@@ -16,6 +16,7 @@ if (!isset($_SESSION['user_email'])) {
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style/home_style2.css">
 </head>
 
 <body>
